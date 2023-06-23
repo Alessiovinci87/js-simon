a6f9284fb173daf0c6644852cc70fa306285974d
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-23 17:55:43 +0200

    Esercizio terminato

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,49 @@
 let numeriCasuali = [];
 
 for (let i = 0; i < 5; i++) {
-  let numeroCasuale = Math.floor(Math.random() * 11); // Genera un numero casuale compreso tra 0 e 10
-  numeriCasuali.push(numeroCasuale); // Aggiunge il numero casuale all'array
+    let numeroCasuale = Math.floor(Math.random() * 11);
+    numeriCasuali.push(numeroCasuale);
 }
 
 let numeriContainer = document.getElementById("numeri-container");
-numeriContainer.textContent = numeriCasuali.join(", ");
+numeriContainer.textContent = "Numeri generati: " + numeriCasuali.join(", ");
 
-setTimeout(function() {
-  numeriContainer.style.display = "none"; // Nasconde i numeri
-  console.log("Sono passati 10 secondi!");
+setTimeout(function () {
+    numeriContainer.style.display = "none";
+    console.log("Sono passati 5 secondi!");
 
-  let numeriUtente = [];
-  for (let i = 0; i < 5; i++) {
-    let numeroUtente = prompt("Digita il " + (i + 1) + "° numero:");
-    numeriUtente.push(numeroUtente);
-  }
+    setTimeout(function () {
+        let numeriUtente = [];
+        for (let i = 0; i < 5; i++) {
+            let numeroUtente = prompt("Digita il " + (i + 1) + "° numero:");
+            numeriUtente.push(Number(numeroUtente));
+        }
 
-  console.log("Numeri inseriti dall'utente:", numeriUtente);
-}, 10 * 1000);
+        console.log("Numeri inseriti dall'utente:", numeriUtente);
+
+        let numeriCorrispondenti = [];
+        let numeriIndovinati = [];
+        for (let i = 0; i < numeriUtente.length; i++) {
+            if (numeriCasuali.includes(numeriUtente[i])) {
+                numeriCorrispondenti.push('<span class="numero-giusto">' + numeriUtente[i] + '</span>');
+                numeriIndovinati.push(numeriUtente[i]);
+            } else {
+                numeriCorrispondenti.push('<span class="numero-sbagliato">' + numeriUtente[i] + '</span>');
+            }
+        }
+
+        console.log("Numeri corrispondenti:", numeriCorrispondenti);
+
+        let risultatoContainer = document.getElementById("risultato-container");
+        risultatoContainer.innerHTML =
+            '<span>Numeri generati: ' +
+            numeriCasuali.join(", ") +
+            "<br>" +
+            "Numeri inseriti dall'utente: " +
+            numeriCorrispondenti.join(", ") +
+            "<br>" +
+            "Numeri indovinati: " +
+            numeriIndovinati.length + " (" + numeriIndovinati.join(", ") + ")" +
+            "</span>";
+    }, 0);
+}, 5 * 1000);

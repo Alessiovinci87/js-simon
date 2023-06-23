d08d9fb872c7a5301f041f8228de5463a26796e6
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-23 15:28:06 +0200

    add prompt utente

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 let numeriCasuali = [];
 
-    for (let i = 0; i < 10; i++) {
-      let numeroCasuale = Math.floor(Math.random() * 11);
-      numeriCasuali.push(numeroCasuale);
-    }
+for (let i = 0; i < 5; i++) {
+  let numeroCasuale = Math.floor(Math.random() * 11); // Genera un numero casuale compreso tra 0 e 10
+  numeriCasuali.push(numeroCasuale); // Aggiunge il numero casuale all'array
+}
 
-    let numeriContainer = document.getElementById("numeri-container");
-    numeriContainer.textContent = numeriCasuali.join(", ");
+let numeriContainer = document.getElementById("numeri-container");
+numeriContainer.textContent = numeriCasuali.join(", ");
 
-    setTimeout(function() {
-        console.log("Sono passati 10 secondi!");
-      }, 10 * 1000);
-      
+setTimeout(function() {
+  numeriContainer.style.display = "none"; // Nasconde i numeri
+  console.log("Sono passati 10 secondi!");
+
+  let numeriUtente = [];
+  for (let i = 0; i < 5; i++) {
+    let numeroUtente = prompt("Digita il " + (i + 1) + "° numero:");
+    numeriUtente.push(numeroUtente);
+  }
+
+  console.log("Numeri inseriti dall'utente:", numeriUtente);
+}, 10 * 1000);
